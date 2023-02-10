dd710cc51a4066f308c5b3eec23f42d483e3a531
zaiky09 <zamilmozamil@outlook.com>
2023-02-10 08:55:44 +0300

    Tests passing

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
-
+class CashRegister
+    attr_reader :discount, :items
+  
+    def initialize(discount=nil)
+      @total = 0
+      @discount = discount
+      @items = []
+      @last_transaction = 0
+    end
+  
+    def total=(amount)
+      @total = amount
+    end
+  
+    def total
+      @total
+    end
+  
+    def add_item(title, price, quantity=1)
+      @total += price * quantity
+      quantity.times { @items << title }
+      @last_transaction = price * quantity
+    end
+  
+    def apply_discount
+      if @discount
+        @total -= @total * @discount / 100
+        "After the discount, the total comes to $#{@total.to_i}."
+      else
+        "There is no discount to apply."
+      end
+    end
+  
+    def void_last_transaction
+      @total -= @last_transaction
+      @last_transaction = 0
+    end
+  end
